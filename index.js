53e8318deb891e29468ddf4f8bb5e13d959ceee1
Abuzbek <68583672+Abuzbek@users.noreply.github.com>
2021-09-04 16:48:10 +0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const createError = require('http-errors');
 const path = require('path')
+const logger = require('morgan');
+const cors = require('cors')
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html')
@@ -12,7 +19,9 @@ app.get('/about', (req, res) => {
 app.get('/order', (req, res) => {
   res.sendFile(__dirname+'/order.html')
 })
-
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
